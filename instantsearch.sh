fe9cae3953f8a4f6ef38acca0045e13b300000a4
paperbenni <paperbenni@gmail.com>
2020-11-09 21:07:39 +0100

    introduce recents list

--- instantsearch.sh
+++ instantsearch.sh
@@ -2,32 +2,86 @@
 
 # file search GUI for plocate
 
-SEARCHSTRING="$(imenu -i 'search file')"
+if [ "$1" = "-s" ]; then
+    echo "settings mode"
+    instantinstall mlocate
+    instantinstall plocate
+    exit
+fi
+
+INCACHE="$HOME/.cache/instantos/instantsearch"
+
+if [ -e "$INCACHE" ]; then
+    SEARCHSTRING="$(echo "recent files" | instantmenu -c -l 1 -bw 10 -q 'enter search term')"
+else
+    SEARCHSTRING="$(echo "" | instantmenu -c -l 1 -bw 10 -q 'enter search term')"
+fi
+
 [ -z "$SEARCHSTRING" ] && exit
 
-# TODO sort by depth
-searchitem() {
-    if grep -q '\.\*' <<<"$1"; then
-        plocate "$1" --limit 2000
-    else
-        plocate -r -i "$1" --limit 2000
+if [ "$SEARCHSTRING" = "recent files" ]; then
+    RECENTFILE=true
+    SEARCHSTRING="$(tac "$INCACHE" | perl -nE '$seen{$_}++ or print' | instantmenu -c -l 20 -bw 10 -q 'recent files')"
+    [ -z "$SEARCHSTRING" ] && exit
+    CHOICE="$SEARCHSTRING"
+else
+    # TODO sort by depth
+    searchitem() {
+        if grep -q '\.\*' <<<"$1"; then
+            echo "searching with regex enabled"
+            plocate -r -i "$1" --limit 2000
+        else
+            plocate -i "$1" --limit 2000
+        fi
+    }
+
+    if [ -z "$RECENTFILE" ]; then
+        SEARCHLIST="$(searchitem "$SEARCHSTRING")"
+        if [ -z "$SEARCHLIST" ]
+        then
+            imenu -m "no results for $SEARCHSTRING"
+            exit
+        fi
+        CHOICE="$(instantmenu -c -l 20 -bw 4 -w -1 -q 'search results' \
+            <<<"$SEARCHLIST")"
+        [ -z "$CHOICE" ] && exit
     fi
-}
+fi
 
-SEARCHLIST="$(searchitem "$SEARCHSTRING")"
-CHOICE="$(instantmenu -c -l 20 -bw 4 -w -1 -q 'search results' \
-    <<<"$SEARCHLIST")"
-
-if [ -d "$CHOICE" ]
-then
+if [ -d "$CHOICE" ]; then
     echo "open in file manager
 open in terminal"
     instantutils open filemanager "$CHOICE" &
     exit
-elif ! [ -e "$CHOICE" ]
-then
+elif ! [ -e "$CHOICE" ]; then
     echo "file not existing"
     exit
 fi
 
+OPENCHOICE="$(echo ">>b File opener
+:y xdg open
+:b rifle
+:b custom
+:r Close" | instantmenu -l 20 -c -h -1 -wm -w -1 -q "$CHOICE")"
 
+[ -z "$OPENCHOICE" ] && exit
+
+case "$OPENCHOICE" in
+*open)
+    xdg-open "$CHOICE"
+    ;;
+*rifle)
+    rifle "$CHOICE"
+    ;;
+*)
+    OPENER="$(instantmenu_path |
+        instantmenu -l 20 -c -h -1 -wm -w -1 -q "$CHOICE")"
+    [ -z "$OPENER" ] && exit
+    $OPENER "$CHOICE" &
+    ;;
+esac
+
+echo "$CHOICE" >>"$INCACHE"
+if [ "$(wc -l "$INCACHE" | grep -o '^[0-9]*')" -gt 500 ]; then
+    sed -i '1,100d' "$INCACHE"
+fi
